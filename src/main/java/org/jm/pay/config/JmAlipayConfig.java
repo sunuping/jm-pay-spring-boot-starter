47bbd44c02f39af8070e3e90e92d36bb398259fb
kong <sunuping@sina.com>
2024-04-26 00:59:35 +0800

    pref 支付宝证书配置

--- src/main/java/org/jm/pay/config/JmAlipayConfig.java
+++ src/main/java/org/jm/pay/config/JmAlipayConfig.java
@@ -1,6 +1,7 @@
 package org.jm.pay.config;
 
 import com.alipay.api.AlipayClient;
+import com.alipay.api.CertAlipayRequest;
 import com.alipay.api.DefaultAlipayClient;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -47,12 +48,44 @@ public class JmAlipayConfig {
      * RSA2
      */
     private String signType;
+    /**
+     * 设置应用公钥证书路径
+     */
+    private String appCertPath;
+    /**
+     * 设置支付宝公钥证书路径
+     */
+    private String alipayCertPath;
+    /**
+     * 设置支付宝根证书路径
+     */
+    private String alipayRootCertPath;
 
 
     private AlipayClient client;
 
     public AlipayClient getClient() {
         if (client == null) {
+            //构造client
+            CertAlipayRequest certAlipayRequest = new CertAlipayRequest();
+            //设置网关地址
+            certAlipayRequest.setServerUrl("https://openapi.alipay.com/gateway.do");
+            //设置应用Id
+            certAlipayRequest.setAppId(this.appid);
+            //设置应用私钥
+            certAlipayRequest.setPrivateKey(this.rsaPrivateKey);
+            //设置请求格式，固定值json
+            certAlipayRequest.setFormat(this.format);
+            //设置字符集
+            certAlipayRequest.setCharset(this.charset);
+            //设置签名类型
+            certAlipayRequest.setSignType(this.signType);
+            //设置应用公钥证书路径
+            certAlipayRequest.setCertPath(this.appCertPath);
+            //设置支付宝公钥证书路径
+            certAlipayRequest.setAlipayPublicCertPath(this.alipayCertPath);
+            //设置支付宝根证书路径
+            certAlipayRequest.setRootCertPath(this.alipayRootCertPath);
             this.client = new DefaultAlipayClient(this.gatewayUrl, this.appid, this.rsaPrivateKey, this.format, this.charset, this.alipayPublicKey, this.signType);
         }
         return client;
